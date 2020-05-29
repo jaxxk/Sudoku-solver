948b98a5e02f72bc2d26f1a695c2499403bcfd46
jaxxk <forwarwick@live.com>
2020-05-29 11:59:42 -0700

    random sudoku clean up

--- gui.py
+++ gui.py
@@ -34,50 +34,59 @@ class MainScreen(tk.Tk):
 class StartPage(tk.Frame):
 	def __init__(self,parent,controller):
 		tk.Frame.__init__(self,parent)
-		# label = tk.Label(self,text="jack",font=LARGEFONT)
-		# label.pack()
+		self.rbutton = tk.Button(self, text="Random Sudoku Solver",command = lambda:controller.show_frame(RandomPage))
+		self.rbutton.pack()
 
-		rbutton = tk.Button(self, text="Random Sudoku Solver",command = lambda:controller.show_frame(RandomPage))
-		rbutton.pack()
-
-		mbutton = tk.Button(self, text="Manual Input",command = lambda:controller.show_frame(ManulPage))
-		mbutton.pack()
+		self.mbutton = tk.Button(self, text="Manual Input",command = lambda:controller.show_frame(ManulPage))
+		self.mbutton.pack()
 
 class RandomPage(tk.Frame):
 	def __init__(self,parent,controller):
 		tk.Frame.__init__(self,parent)
-		label = tk.Label(self,text="Random Sudoku",font=LARGEFONT)
-		label.pack()
+		self.label = tk.Label(self,text="Random Sudoku",font=LARGEFONT)
+		self.label.pack()
 
-		showButton = tk.Button(self, text="Show Solution",command = self.showRes)
-		showButton.pack()
+		self.showButton = tk.Button(self, text="Show Solution",command = self.showRes)
+		self.showButton.pack()
+
+		self.eraseButton = tk.Button(self, text="Erase",command =  self.delete)
+		self.eraseButton.pack()
+		self.returnButton = tk.Button(self, text="Main Meanu",command = lambda:controller.show_frame(StartPage))
+		self.returnButton.pack()
 		
-		eraseButton = tk.Button(self, text="Erase",command =  self.delete)
-		eraseButton.pack()
-
-		returnButton = tk.Button(self, text="Main Meanu",command =  lambda:controller.show_frame(StartPage))
-		returnButton.pack()
-
+		self. dele = 0
 		p = sub.Popen('test',stdout=sub.PIPE,stderr=sub.PIPE)
 		self.output, errors = p.communicate()
-		self.text = Text(self)
-	def delete(self):
-		self.text.delete('1.0', END)
+
 
 	def showRes(self):
+		self.text = Text(self)
 		self.text.pack()
-		self.text.insert(END, self.output)
+		self.text.insert(END,self.output)
+	def delete(self):
+	 	self.dele = 1
+	 	self.text.pack_forget()
+	 	self.eraseButton.pack_forget()
 	
-
-
 class ManulPage(tk.Frame):
 	def __init__(self,parent,controller):
 		tk.Frame.__init__(self,parent)
-		label = tk.Label(self,text="Manual Sudoku",font=LARGEFONT)
-		label.pack()
+		self.label = tk.Label(self,text="Manual Sudoku",font=LARGEFONT)
+		self.label.pack()
+		# self.grid()
+  #       self.create_widgets()
 
-		returnButton = tk.Button(self, text="Back to home",command = lambda:controller.show_frame(StartPage))
-		returnButton.pack()
+	# def create_widgets(self):
+ #        self.entries = {}
+ #        self.tableheight = 9
+ #        self.tablewidth = 9
+ #        counter = 0
+ #        for row in xrange(self.tableheight):
+ #            for column in xrange(self.tablewidth):
+ #                self.entries[counter] = Entry(self, width=5)
+ #                self.entries[counter].grid(row=row, column=column)
+ #                counter += 1
+
 
 app = MainScreen()
 app.mainloop()
