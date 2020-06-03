f5bd5131b44d1442791ab2d3ff093336028c94e7
mello-code <48005787+mello-code@users.noreply.github.com>
2020-06-02 22:59:31 -0700

    ocd

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -1,6 +1,5 @@
 from ctypes import windll, byref, create_unicode_buffer, create_string_buffer
 from lxml import html
-import tkinter as tk
 from tkinter import *
 from tkinter import messagebox
 from playsound import playsound
@@ -25,7 +24,7 @@ app = Tk()
 
 class mainScreen:
     altered, board, cells, cursor, original, solved = ([] for i in range(6))
-    levels = ["Beginner", "Intermediate", "Advanced", "Expert", "Master"]
+    levels = ["Beginner", "Intermediate                    ", "Advanced", "Expert", "Master"]
     psms = [0,1,2,3,4,5,6,7,8,9,10,11,12,13]
     selected = () # x, y
 
@@ -283,7 +282,7 @@ class mainScreen:
         paddingLabel.place(x = 381, y = buttonYOffset + (buttonHeight + buttonPadding) * 6 + buttonGap * 2 + 54)
         paddingBD = Frame(app, bd = 0, highlightbackground = "#CCCCCC", highlightthickness = 1, width = 50, height = buttonHeight)
         paddingBD.place(x = 440, y = buttonYOffset + (buttonHeight + buttonPadding) * 6 + buttonGap * 2 + 54)
-        textBox = tk.Text(paddingBD,height = 2,width = 50)
+        textBox = Text(paddingBD,height = 2,width = 50)
         textBox.place(x = 0, y = 0)
 
 
