d6f57c8a4a7f966b57ea58fa112ba640cae8b8c2
jaxxk <forwarwick@live.com>
2020-06-04 18:57:21 -0700

    testing

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -4,7 +4,7 @@ from tkinter import *
 from tkinter import messagebox
 from playsound import playsound
 from PIL import Image
-import os
+from test import *
 
 from sudokuAlgorithm import *
 from sudokuBoard import *
@@ -397,6 +397,6 @@ def main():
     app.mainloop()
 
 if __name__=="__main__": 
-    cmd = "SF-Pro-Display-Regular.otf"
-    os.system(cmd)
+    path = (r"assets\fonts\SF-Pro-Display-Regular.otf")
+    install_font(path)
     main()
