66bf098b828cf25d1d3aeeff588fe9fa4a13989f
mello-code <48005787+mello-code@users.noreply.github.com>
2020-06-04 19:33:52 -0700

    Revert "test"
    This reverts commit 710570b246c25f9fa16c58642d768fadc4ed7b04, reversing
    changes made to d6f57c8a4a7f966b57ea58fa112ba640cae8b8c2.

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -4,10 +4,7 @@ from tkinter import *
 from tkinter import messagebox
 from playsound import playsound
 from PIL import Image
-<<<<<<< HEAD
 from test import *
-=======
->>>>>>> refs/remotes/origin/master
 
 from sudokuAlgorithm import *
 from sudokuBoard import *
@@ -284,11 +281,6 @@ class mainScreen:
     # main
     def __init__(self, root):
         # Grab a font from path to use (needs replacement)
-        pathbuf = create_unicode_buffer(
-            "assets\\fonts\\SF-Pro-Display-Regular.otf")
-        AddFontResourceEx = windll.gdi32.AddFontResourceExA
-        AddFontResourceEx(byref(pathbuf), 0x10, 0)
-
         # Title and Github Button
         titleLabel = Label(app, text = "SudokuSolver", font = ("SF Pro Display", 17))
         titleLabel.place(x = 15, y = 15)
@@ -407,5 +399,4 @@ def main():
 if __name__=="__main__": 
     path = (r"assets\fonts\SF-Pro-Display-Regular.otf")
     install_font(path)
-
     main()
