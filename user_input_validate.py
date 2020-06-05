73df6de4a8d45dec7460dfcbe5e97bbd8c76084f
fluffysheepcloud <47491959+fluffysheepcloud@users.noreply.github.com>
2020-06-04 23:17:01 -0700

    Add files via upload
    changed the return statements into a list appending duplicate locations in the form of a tuple

--- user_input_validate.py
+++ user_input_validate.py
@@ -1,21 +1,21 @@
 #potent number is now a user input number
 #checking if that user num value already exists within the row, col, grid
-#if so return the number location to "flag" that number location so user knows what number not to use
-#return duplicate's coordinates and user inputs if there are duplicates
+#if so add it onto empty list location_list which will hold all the x,y locations of duplicates
 def validate(megalist, user_number, x, y):
+    location_list = []
     # check specified row that is determined thru value x
     row_check = user_number
     for g in range(9):
         if row_check == megalist[x][g]:
-            # duplicate found in row --> return duplicate coords (w,v) and user input coord (x,y)
-            return (x,g),(x,y)
+            # adding tuple of row duplicate (x,g)
+            location_list.append((x,g))
 
     # check specified col that is determined thru value y
     col_check = user_number
     for p in range(9):
         if col_check == megalist[p][y]:
-            # duplicate found in column --> return duplicate coords (w,v) and user input coord (x,y)
-            return (p,y), (x,y)
+            # adding tuple of column duplicate (p,y)
+            location_list.append((p,y))
 
     # check grid
     modX = x // 3
@@ -24,5 +24,7 @@ def validate(megalist, user_number, x, y):
     for w in range(modX * 3, modX * 3 + 3):
         for v in range(modY * 3, modY * 3 + 3):
             if grid_check == megalist[w][v]:
-                #duplicate found in grid --> return duplicate coords (w,v) and user input coord (x,y)
-                return (w,v), (x,y)
+                # adding tuple of grid duplicate (w,v)
+               location_list.append(w,v)
+                
+    return location_list
