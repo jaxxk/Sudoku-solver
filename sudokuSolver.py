72f41b2a91e6e17e413f74eb8322ab3e145aaab5
mello-code <48005787+mello-code@users.noreply.github.com>
2020-06-04 18:48:46 -0700

    Revert "downloading fonts"
    This reverts commit be058b8414b23a11d3db9e185eec306664d37b34.

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -4,7 +4,6 @@ from tkinter import *
 from tkinter import messagebox
 from playsound import playsound
 from PIL import Image
-import os
 
 from sudokuAlgorithm import *
 from sudokuBoard import *
@@ -281,6 +280,11 @@ class mainScreen:
     # main
     def __init__(self, root):
         # Grab a font from path to use (needs replacement)
+        pathbuf = create_unicode_buffer(
+            "assets\\fonts\\SF-Pro-Display-Regular.otf")
+        AddFontResourceEx = windll.gdi32.AddFontResourceExA
+        AddFontResourceEx(byref(pathbuf), 0x10, 0)
+
         # Title and Github Button
         titleLabel = Label(app, text = "SudokuSolver", font = ("SF Pro Display", 17))
         titleLabel.place(x = 15, y = 15)
@@ -397,6 +401,4 @@ def main():
     app.mainloop()
 
 if __name__=="__main__": 
-    cmd = "SF-Pro-Display-Regular.otf"
-    os.system(cmd)
     main()
